fe419a1eca7104352ad58469b0757ab78b0113e6
Dominic <dominic.zedler.one@gmail.com>
2018-05-21 03:00:48 +0200

    initial version

---
+++ action-searchWeatherForecastTemperature-Wetter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+import random  # for random answer forms
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    user,intentname = intentMessage.intent.intent_name.split(':')  # the user can fork the intent with this method
+    if intentname == "searchWeatherForecastTemperature":
+        conf = read_configuration_file(CONFIG_INI)
+        hermes.publish_end_session(intentMessage.session_id, random.choice(weather.forecast_temperature(intentMessage)))
+
+
+if __name__ == "__main__":
+    conf = read_configuration_file(CONFIG_INI)
+    wether = Weather(conf)
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intents(subscribe_intent_callback).start()
