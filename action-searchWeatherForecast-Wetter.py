3bc65af89cd0588ab074c40e8a3c4ef20dcd684c
Dominic <dominic.zedler.one@gmail.com>
2018-05-22 13:15:45 +0200

    added logging code

--- action-searchWeatherForecast-Wetter.py
+++ action-searchWeatherForecast-Wetter.py
@@ -17,13 +17,13 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
 
 
 def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
+    #try:
+    with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+        conf_parser = SnipsConfigParser()
+        conf_parser.readfp(f)
+        return conf_parser.to_dict()
+    #except (IOError, ConfigParser.Error) as e:
+    #    return dict()
 
 def subscribe_intent_callback(hermes, intentMessage):
     user,intentname = intentMessage.intent.intent_name.split(':')  # the user can fork the intent with this method
